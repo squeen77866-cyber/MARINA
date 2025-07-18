6fda55cf4d3a8b7d2f8bca83e02c6fef5cae4e1d
Ahadmughalking <aliahadmughal789@gmail.com>
2025-07-18 15:14:25 +0500

    Update mention.js

--- Priyansh/commands/mention.js
+++ Priyansh/commands/mention.js
@@ -13,7 +13,7 @@ module.exports.handleEvent = function({ api, event }) {
     var aid = ["100027055397691"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
-      var msg = ["Abee Mere se baat Kar ..Chuza Boss Ko mention kyu kar rhe ho😒" , "Kya Hua Chuza Boss ko q Bula Rhe Ho?" , "Wo Shayad Busy h😐🙄" , "Tujhe sunai nhi deta mere admin ko tang na kr😒😒" , "Mere Chuza boss ko tang naa karo" , "Abee Bola na mere Admin ko bar bar mention mat karo ek bar me smjh nahi ata" , "Kya kaam hai mujhe bol" , "Ek Baar me samjh ni ata kyu mention kar rhe ho Chuza Boss Ko" ];
+      var msg = ["Abee Mere se baat Kar ..Chuza Boss Ko mention kyu kar rhe ho😒" , "Kya Hua Chuza Boss ko q Bula Rhe Ho?" , "Ahad boss ko distrub nahi kro🤨🙄" , "Wo Shayad Busy h😐🙄" , "Tujhe sunai nhi deta mere admin ko tang na kr😒😒" , "Mere Chuza boss ko tang naa karo" , "Abee Bola na mere Admin ko bar bar mention mat karo ek bar me smjh nahi ata" , "Kya kaam hai mujhe bol" , "Ek Baar me samjh ni ata kyu mention kar rhe ho Chuza Boss Ko" ];
       return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
     }
     }}
