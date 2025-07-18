5a3dc5c61c1ffd16323480b25063e2ef1f040489
Ahadmughalking <aliahadmughal789@gmail.com>
2025-07-18 15:18:12 +0500

    Update owner.js

--- Priyansh/commands/owner.js
+++ Priyansh/commands/owner.js
@@ -24,16 +24,16 @@ const fs = global.nodemodule["fs-extra"];
      ];
      var callback = () => api.sendMessage({body:`🔰𝙊𝙒𝙉𝙀𝙍 𝙄𝙉𝙁𝙊🔰
 
- ཫ༄𒁍⃝𝐌𝐀𝐑⃟𝐈𝐍𝐀•◡•)
+ ཫ༄⍣⃝𝐀𝐇𝐀𝐃 𝐌𝐔𝐆𝐇𝐀𝐋⟭𓆪•)
 
-𝐀𝐠𝐞 : 🙈
+𝐀𝐠𝐞 : 21🙈
 
-𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 𝐖𝐢𝐭𝐡 : 🙈🙈
+𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 𝐖𝐢𝐭𝐡 : تــــلاش بــــراۓشــــونــی بـــچــی 🍿💜🎻
 
 
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 https://www.facebook.com
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 https://www.facebook.com/share/16k5Ywk9cj/
 
-𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 : 🙈🙈🙈
+𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 : NI DO GA 😂
 
  🚩🌍❤️🙂!❤🙂♣️`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
